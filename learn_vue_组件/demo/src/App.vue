04a83fc47b357fbd13e12b301e62e1a27dc7a807
Qin Yihua <qinyh10300@gmail.com>
2025-05-18 02:02:48 +0800

    add 子传父

--- learn_vue_组件/demo/src/App.vue
+++ learn_vue_组件/demo/src/App.vue
@@ -1,28 +1,28 @@
 <script setup>
-    import { reactive } from 'vue'
+    import { reactive, ref } from 'vue'
     import Header from './components/header.vue'
-    import Footer from './components/footer.vue'
 
     // 响应式数据才会影响页面渲染
-    const propsWeb = reactive({
+    const web = reactive({
         user: 10,
         url: "google.com"
     })
 
-    const userAdd = () => {
-        propsWeb.user++
-        console.log(propsWeb.user);
+    const emitsGetWeb = (data) => {
+        console.log(data)
+        web.url = data.url
+    }
+
+    const emitsUserAdd = (data) => {
+        console.log(data)
+        web.user += data
     }
 </script>
 
 <template>
-    <Header propsName="秦一骅" propsUrl="google.com"/>
-    秦一骅
-    <h1>秦一骅</h1>
+    <Header @getWeb="emitsGetWeb" @userAdd="emitsUserAdd"/>
 
-    <button @click="userAdd">添加用户</button>
-
-    <Footer :="propsWeb"/>
+    {{ web.url }} - {{ web.user }}
 </template>
 
 <style scoped>
--- learn_vue_组件/demo/src/components/header.vue
+++ learn_vue_组件/demo/src/components/header.vue
@@ -1,10 +1,16 @@
 <script setup>
-const props = defineProps(["propsName", "propsUrl"])
-    console.log(props);
+    const emits = defineEmits(["getWeb", "userAdd"])
+    emits("getWeb", { name: "秦一骅", url: "qinyh10300.com" })
+
+    const add = () => {
+        emits("userAdd", 10)
+    }
 </script>
 
 <template>
     <h3>Header</h3>
+
+    <button @click="add">添加用户</button><br>
 </template>
 
 <style scoped>
