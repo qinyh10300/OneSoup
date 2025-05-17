e4fdf6e753aa7243877e020726cde9c03a03c899
Qin Yihua <qinyh10300@gmail.com>
2025-05-17 16:34:05 +0800

    add vite+vue组件

---
+++ learn_vue_组件/demo/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+    import Header from './components/header.vue'
+    import Footer from './components/footer.vue'
+</script>
+
+<template>
+    <Header/>
+    秦一骅
+    <h1>秦一骅</h1>
+    <Footer/>
+</template>
+
+<style scoped>
+
+</style>
