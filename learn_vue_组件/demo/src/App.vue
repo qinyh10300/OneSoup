de5251d947055310fdbaf391a3603a4e46e8620f
Qin Yihua <qinyh10300@gmail.com>
2025-05-18 02:45:02 +0800

    add 生命周期函数

--- learn_vue_组件/demo/src/App.vue
+++ learn_vue_组件/demo/src/App.vue
@@ -1,28 +1,23 @@
 <script setup>
-import Header from './components/header.vue'
-import Footer from './components/footer.vue'
+import { onMounted, onUpdated, ref } from 'vue'
+
+onMounted(() => {
+    console.log("onMounted");
+})
+
+onUpdated(() => {
+    console.log("onUpdated");
+})
+
+const user = ref(0)
+console.log(user.value);
+
 </script>
 
 <template>
-    <h3>App.vue</h3>
+    {{ user }}
 
-    <Header>
-        <!-- 匿名插槽 -->
-        <a href="qinyh10300.github.io">秦一骅</a>
-    </Header>
-
-    <Footer>
-        <!-- 具名插槽 -->
-        <!-- <template v-slot:url> -->
-        <!-- <template #url="data"> -->
-        <!-- 可以使用data形式 -->
-        <template #url="{title, user}">
-        <!-- 也可以使用解构形式 -->
-            {{ title }}
-            {{ user }}
-            <a href="www.baidu.com">网址</a>
-        </template>
-    </Footer>
+    <button @click="user++">添加用户</button>
 </template>
 
 <style scoped>
